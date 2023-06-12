739e7bbbd92c8eba924c484fbd60492435e103aa
Shima <shimaadnanbasmenj@gmail.com>
2023-06-12 13:41:35 +0100

    first python attempt

--- kyu8/Kyu6/fibonachi.js
+++ kyu8/Kyu6/fibonachi.js
@@ -1,10 +1,19 @@
 function nthFibo(n) {
     // Return the n-th number in the Fibonacci Sequence
-    if(n<2){
-      return 0
+
+    // if(n<2){
+    //   return 0
+    // }
+    // if(n==2){return 1}
+
+    // else{
+    //   return nthFibo(n-1)+nthFibo(n-2)
+    // }
+    let[a,b]=[0,1]
+    let res=[]
+    for(let i=0;i<n;i++){
+        [a,b]=[b,a+b]
+        res.push(a)
     }
-    if(n==2){return 1}
-    else{
-      return nthFibo(n-1)+nthFibo(n-2)
-    }
+    return res[n-1]
   }
