c3cc9099c51c655d4ce9e8dae661a609ccb34479
Shima <shimaadnanbasmenj@gmail.com>
2023-06-26 13:51:48 +0100

    the first 2 item with sum

--- kyu8/Kyu5/The first 2 item with the sum.js
+++ kyu8/Kyu5/The first 2 item with the sum.js
@@ -1,8 +1,9 @@
 var sum_pairs=function(ints, s){
     var seen = {}
     for (var i = 0; i < ints.length; ++i) {
-      if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
+      if (seen[s - ints[i]]) console.log(`result=${[s - ints[i], ints[i]]}`); 
       seen[ints[i]] = true
+      console.log(seen);
     }
   }
   console.log(sum_pairs([10,5,2,3,7,5],10));
