0948c967602980e8962d589a3c279c962cb1e71b
Shima <shimaadnanbasmenj@gmail.com>
2023-07-11 12:18:38 +0100

    JS magics

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ let x = 7;
 x++;
 x - 2;
 x *= 3;
-console.log(x);
+// console.log(x);
+function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+  }
+  
+  console.log(addThreeNumbers(1, 2, 3, 4)); // => ???
+  //JavaScript Magic
+  function isCool(string) {
+    let coolSentence = string + " is cool!"
+    console.log(coolSentence)
+    return coolSentence
+  }
