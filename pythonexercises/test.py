9ab15f003ec8d0f2be879c60e4a1ea432ca0a26a
Shima <shimaadnanbasmenj@gmail.com>
2023-07-14 10:34:20 +0100

    title in py

--- pythonexercises/test.py
+++ pythonexercises/test.py
@@ -2,3 +2,10 @@ n = 1024
 while n > 0:
   print (n % 10)
   n = n // 10
+  # title
+  def format_name(f_name,l_name):
+    f_name=f_name.title()
+    l_name=l_name.title()
+    return f_name + l_name
+
+print(format_name('shima','adnan'))
