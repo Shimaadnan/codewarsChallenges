654598ea94bb2a53ffc77e03bc258c9d0dc587b7
Shima <shimaadnanbasmenj@gmail.com>
2023-09-12 12:33:31 +0100

    the multiples of 5 that are less than the max.

--- kyu8/Take the Derivative.js
+++ kyu8/Take the Derivative.js
@@ -14,3 +14,13 @@ function logBetweenWhile(lowNum, highNum) {
       i++;
     }
   }
+  // the multiples of 5 that are less than the max.
+  function printFives1(max) {
+    let i = 0;
+    while (i < max) {
+      if (i % 5 === 0) {
+        console.log(i);
+      }
+      i++;
+    }
+  }
